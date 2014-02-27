cf68516ef207c1f1e44821ee8b772bd6c292d65c
MaPePeR <mapeper@example.com>
2014-02-27 22:03:04 +0100

    Colorblind mode only allowing IceSnow1, LavaGrotto and SnowyMountain (Problems to see Red/Green/Yellow)

--- NoIceBackgroundMod.cs
+++ NoIceBackgroundMod.cs
@@ -27,12 +27,22 @@ namespace NoIceBackgroundMod
 
 		public override void AfterInvoke (InvocationInfo info, ref object returnValue) {
 			if (returnValue.GetType().Equals(typeof(int))) {
-				if ((int)returnValue == 3) // IceSnow1
-					returnValue =  1; //GrassyMountain
-				if ((int)returnValue == 5) //SnowyMountain
-					returnValue =  2;//GreenMeadow
-				if ((int)returnValue == 6) //YellowMeadow
-					returnValue = 4;//LavaGrotto
+				//Accepted: Ice,  lava, snow
+				if ((int)returnValue == 3  || (int)returnValue == 4 || (int)returnValue == 5) {
+					//Nothing to do
+				} else {
+					if ((int)returnValue == 0) // DeepForest
+						returnValue = 3; //IceSnow1
+					else if ((int)returnValue == 1) // GrassyMountain
+						returnValue = 4; //LavaGrotto
+					else if ((int)returnValue == 2) //GreenMeadow
+						returnValue = 4;//LavaGrotto
+					else if ((int)returnValue == 6) //YellowMeadow
+						returnValue = 5;//SnowyMountain
+					else
+						returnValue = 5; //SnowyMountain //This should not happen
+				}
+
 			}
 			//HardCoded - but yea...
 		}
